07f2bceb6018268fc0b0598b2ca9834e42ec5f9e
pistolove <pistolove@163.com>
2015-06-05 16:36:49 +0800

    外观模式

---
+++ src/com/design/facade/Amplifier.java
@@ -0,0 +1,21 @@
+package com.design.facade;
+
+public class Amplifier {
+	private Tuner tuner;
+	private Dvdplayer dvdplayer;
+	private Cdplayer dCdplayer;
+	
+	
+	private void on(){
+		
+	}
+	
+	private void off(){
+		
+	}
+	
+	
+	
+	
+	
+}
---
+++ src/com/design/facade/Tuner.java
@@ -0,0 +1,5 @@
+package com.design.facade;
+
+public class Tuner {
+
+}
