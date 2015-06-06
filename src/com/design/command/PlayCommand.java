e90fe0b0b4c27e944bf08fcf6e88292ea6f7ba98
pistolove <pistolove@163.com>
2015-06-06 15:19:55 +0800

    命令模式

---
+++ src/com/design/command/PlayCommand.java
@@ -0,0 +1,18 @@
+package com.design.command;
+
+//具体命令角色
+public class PlayCommand implements Command {
+	private AudioPlayer audioPlayer;
+	
+	public PlayCommand(AudioPlayer player){
+		this.audioPlayer  = player;
+	}
+	
+	
+	@Override
+	public void excute() {
+		audioPlayer.play();
+	}
+
+
+}
