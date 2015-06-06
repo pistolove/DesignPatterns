f3780bba54182119e506957cf0f9e7e923a52ad5
pistolove <pistolove@163.com>
2015-06-06 23:10:01 +0800

    状态模式

---
+++ src/com/design/state/ConcreateStateA.java
@@ -0,0 +1,10 @@
+package com.design.state;
+
+public class ConcreateStateA implements State {
+
+	@Override
+	public void handle(String statePram) {
+		System.err.println("开始执行" + statePram);
+	}
+
+}
